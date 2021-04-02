3a9ab532b6fa477187e6072a3abc0a787f7d4e6d
Josué Jaramillo <josuews303@hotmail.com>
2021-04-02 18:01:03 -0500

    Deployable

---
+++ src/build/precache-manifest.057f4503065d906c88da9bd91c820048.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0d52f0b2b1e39b5eb2d",
+    "url": "/static/css/main.07c82433.chunk.css"
+  },
+  {
+    "revision": "f0d52f0b2b1e39b5eb2d",
+    "url": "/static/js/main.106953fa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d72546995997ab1773b6",
+    "url": "/static/js/2.f96445f7.chunk.js"
+  },
+  {
+    "revision": "a55bc0cfd04223008711cd6a9a310f9c",
+    "url": "/index.html"
+  }
+];
